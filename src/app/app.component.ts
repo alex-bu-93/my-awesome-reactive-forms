16a01df4f17cf22c64be03d9ddeb3b72fa37d941
Aleksandr <abukanov1993@gmail.com>
2019-03-02 20:19:24 +0300

    Step 2 - Classical reactive form added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component }                          from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'my-awesome-reactive-forms';
+
+  EMAIL_PATTERN = '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}$';
+  CYRILLIC_PATTERN = '^[А-Яа-яЁё\\s]+$';
+
+  feedbackForm: FormGroup = new FormGroup({
+    'userName': new FormControl(
+      null,
+      [Validators.required, Validators.pattern(this.CYRILLIC_PATTERN)]),
+    'userEmail': new FormControl(
+      null,
+      [Validators.required, Validators.pattern(this.EMAIL_PATTERN)])
+  });
+
+  onSubmit(): void {
+    if (this.feedbackForm.valid) {
+      // Working on your validated form data
+    } else {
+      this.markAllFormFieldsAsTouched(this.feedbackForm);
+    }
+  }
+
+  markAllFormFieldsAsTouched(formGroup: FormGroup): void {
+    Object.keys(formGroup.controls).forEach(
+      (field) => {
+        const control = formGroup.get(field);
+        if (control instanceof FormControl) {
+          control.markAsTouched({onlySelf: true});
+        } else if (control instanceof FormGroup) {
+          this.markAllFormFieldsAsTouched(control);
+        }
+      }
+    );
+  }
 }
