69edcc1d100beff35384aef090ea8dff1b58e98d
Aleksandr <abukanov1993@gmail.com>
2019-03-02 20:19:01 +0300

    Step 1 - App created

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule }       from '@angular/platform-browser';
+import { NgModule }            from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent }     from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {
+}
